9f3dcca7cd09dddcad381249db8683e6acb9765d
Divya A K <akdivya603@gmail.com>
2026-03-03 18:46:59 +0530

    Added SQL practice file

---
+++ day 1.sql
@@ -0,0 +1,26 @@
+-- challenge 1
+show databases;
+create database company_db;
+use company_db;
+create table employees(
+emp_id smallint,
+emp_name varchar(50),
+salary decimal(10,2)
+);
+-- describ the structure of table
+desc employees;
+
+-- challenge 2
+alter table employees 
+add department varchar(20);
+alter table employees 
+modify salary decimal(15,2);
+desc employees;
+
+-- challenge 3
+alter table employees
+drop department;
+alter table employees 
+add experience smallint;
+desc employees;
+
